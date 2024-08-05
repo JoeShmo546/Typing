f6c502eb38394b74f3f0a38ef896c819f161c47f
joe shmo <omicron@omicron.home>
2024-08-05 16:40:54 -0500

    get random word

--- main.java
+++ main.java
@@ -1,10 +1,49 @@
 import java.util.*;
+import java.io.*;
+
+/*
+ *  get a random word from a file
+ *  add it to a buffer
+ *  collect user input 
+ *  compare user input to the first word in the buffer
+ *
+ */
 
 public class main {
+
+    public static String file = "words.txt";
+
+    public static String getWord(){
+
+        Random random = new Random();
+        int lineNumber = random.nextInt(5459);
+
+        try(BufferedReader br = new BufferedReader(new FileReader (file))) {
+
+            String line;
+            int currentLine = 1;
+
+            while((line = br.readLine()) != null){
+                if(currentLine == lineNumber) {
+                    return line;
+                }
+                currentLine++;
+            }
+        } catch (IOException e){
+            System.err.println("An error ocurred while reading the file.");
+            //e.printStackTrack();
+        }
+        return "nothing";
+
+    }
+
     public static void main(String[] args){
+        
+        for (int i = 0; i < 10; i++){
+            System.out.print(getWord() + " ");
+        }
+        System.out.println();
 
-
-        System.out.println("Hellow world");
 
     }
 }
